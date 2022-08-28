9958d8be17548dfee0a9264d417400bd910e22d5
Emmanuel Agüero <54730752+EmmanuelAR@users.noreply.github.com>
2022-08-28 11:20:33 -0600

    Add files via upload
    Inicial commit

---
+++ farmastock/src/main/kotlin/com/globales/farmastock/FarmastockApplication.kt
@@ -0,0 +1,11 @@
+package com.globales.farmastock
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class FarmastockApplication
+
+fun main(args: Array<String>) {
+	runApplication<FarmastockApplication>(*args)
+}
