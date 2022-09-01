5c946c9ce0666dcd082b8a6b0a5bc74741ad9d3b
EmmanuelAR <54730752+EmmanuelAR@users.noreply.github.com>
2022-09-01 11:00:22 -0600

    WEB SERVICES DE USER-ROLE-PRIVILEGE *no se ha probado,faltan objetos*

---
+++ farmastock/src/main/kotlin/com/globales/farmastock/Webservice.kt
@@ -0,0 +1,118 @@
+package com.globales.farmastock
+
+import org.springframework.http.MediaType
+import org.springframework.web.bind.annotation.*
+
+//      PRIVILEGE
+
+@RestController
+@RequestMapping("\${url.privileges}")
+class PrivilegesController(private val privilegeService: PrivilegeService) {
+
+    @GetMapping
+    @ResponseBody
+    fun findAll() = privilegeService.findAll()
+
+    @Throws(NoSuchElementException::class)
+    @GetMapping("{id}")
+    @ResponseBody
+    fun findById(@PathVariable id: Long) = privilegeService.findById(id)
+
+
+    @PostMapping(consumes = [MediaType.APPLICATION_JSON_VALUE], produces = [MediaType.APPLICATION_JSON_VALUE])
+    @ResponseBody
+    fun create(@RequestBody privilegeInput: PrivilegeInput): PrivilegeDetails? {
+        return privilegeService.create(privilegeInput)
+    }
+
+    @Throws(NoSuchElementException::class)
+    @PutMapping(consumes = [MediaType.APPLICATION_JSON_VALUE], produces = [MediaType.APPLICATION_JSON_VALUE])
+    @ResponseBody
+    fun update(@RequestBody privilegeInput: PrivilegeInput): PrivilegeDetails? {
+        return privilegeService.update(privilegeInput)
+    }
+
+    @Throws(NoSuchElementException::class)
+    @DeleteMapping("{id}")
+    @ResponseBody
+    fun deleteById(@PathVariable id: Long) {
+        privilegeService.deleteById(id)
+    }
+}
+
+//      ROLE
+@RestController
+@RequestMapping("\${url.roles}")
+class RoleController(private val roleService: RoleService) {
+
+    @GetMapping
+    @ResponseBody
+    fun findAll() = roleService.findAll()
+
+    @Throws(NoSuchElementException::class)
+    @GetMapping("{id}")
+    @ResponseBody
+    fun findById(@PathVariable id: Long) = roleService.findById(id)
+
+
+    @PostMapping(consumes = [MediaType.APPLICATION_JSON_VALUE], produces = [MediaType.APPLICATION_JSON_VALUE])
+    @ResponseBody
+    fun create(@RequestBody roleInput: RoleInput): RoleDetails? {
+        return roleService.create(roleInput)
+    }
+
+    @Throws(NoSuchElementException::class)
+    @PutMapping(consumes = [MediaType.APPLICATION_JSON_VALUE], produces = [MediaType.APPLICATION_JSON_VALUE])
+    @ResponseBody
+    fun update(@RequestBody roleInput: RoleInput): RoleDetails? {
+        return roleService.update(roleInput)
+    }
+
+    @Throws(NoSuchElementException::class)
+    @DeleteMapping("{id}")
+    @ResponseBody
+    fun deleteById(@PathVariable id: Long) {
+        roleService.deleteById(id)
+    }
+}
+
+//      USER
+@RestController
+@RequestMapping("\${url.users}")
+class UserController(private val userService: UserService) {
+
+    @GetMapping
+    @ResponseBody
+    fun findAll() = userService.findAll()
+
+    @Throws(NoSuchElementException::class)
+    @GetMapping("{id}")
+    @ResponseBody
+    fun findById(@PathVariable id: Long) = userService.findById(id)
+
+    @Throws(NoSuchElementException::class)
+    @GetMapping("email/{username}")
+    @ResponseBody
+    fun findByEmail(@PathVariable username: String) = userService.findByEmail(username)
+
+
+    @PostMapping(consumes = [MediaType.APPLICATION_JSON_VALUE], produces = [MediaType.APPLICATION_JSON_VALUE])
+    @ResponseBody
+    fun create(@RequestBody userInput: UserInput): UserResult? {
+        return userService.create(userInput)
+    }
+
+    @Throws(NoSuchElementException::class)
+    @PutMapping(consumes = [MediaType.APPLICATION_JSON_VALUE], produces = [MediaType.APPLICATION_JSON_VALUE])
+    @ResponseBody
+    fun update(@RequestBody userInput: UserInput): UserResult? {
+        return userService.update(userInput)
+    }
+
+    @Throws(NoSuchElementException::class)
+    @DeleteMapping("{id}")
+    @ResponseBody
+    fun deleteById(@PathVariable id: Long) {
+        userService.deleteById(id)
+    }
+}
